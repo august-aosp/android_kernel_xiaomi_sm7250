54bb8c01270cc261672a082b4eb103f648a77245
Valentin Schneider <valentin.schneider@arm.com>
2020-02-06 19:19:56 +0000

    sched/core: Remove for_each_lower_domain()
    The last remaining user of this macro has just been removed, get rid of it.
    
    Suggested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Quentin Perret <qperret@google.com>
    Link: https://lkml.kernel.org/r/20200206191957.12325-4-valentin.schneider@arm.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1567,8 +1567,6 @@ queue_balance_callback(struct rq *rq,
 	for (__sd = rcu_dereference_check_sched_domain(cpu_rq(cpu)->sd); \
 			__sd; __sd = __sd->parent)
 
-#define for_each_lower_domain(sd) for (; sd; sd = sd->child)
-
 /**
  * highest_flag_domain - Return highest sched_domain containing flag.
  * @cpu:	The CPU whose highest level of sched domain is to
